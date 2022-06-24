ee153e1cf8556fe2959f65c86cf992a5c0d6cc1c
AdelarnyFEB <abidemiadelani@gmail.com>
2022-06-24 19:42:48 +0100

    js

---
+++ style.js
@@ -0,0 +1,5 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+alert("Name: Adelani Abidemi Abideen");
+alert("Height: 162cm");
+alert("Country: Nigeria");
+ 
